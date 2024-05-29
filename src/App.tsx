0b77005dc7aa7fbc0714260aaf63024b658a250f
Jason Henriquez <jasonhenriquez@protonmail.com>
2024-05-28 23:24:47 -0500

    Implement scroll position going to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom'
 import type { RouteObject } from "react-router-dom";
 import './App.css'
 import Home from './views/Home/Home';
@@ -96,11 +96,15 @@ const routeObject: RouteObject = {
 const router = createBrowserRouter([routeObject]);
 function LanguageLoader(props: ILanguageLoader) {
   const { i18n } = useTranslation(['common']);
+  const { pathname } = useLocation();
   
   useEffect(() => {
     const newLanguage: string = props.lang === 'fr' ? 'fr' : 'en';
     void i18n.changeLanguage(newLanguage);
-  }, [props.lang, i18n]);
+
+    // scroll to top on route change
+    window.scrollTo(0, 0);
+  }, [props.lang, i18n, pathname]);
 
   return (props.children)
 }
